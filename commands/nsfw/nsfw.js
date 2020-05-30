2bf006fb3ffbeb17634c98fadd4e9184435690e0
Sean Doran <sdoran@iastate.edu>
2020-05-30 00:14:07 -0500

    Condensed many NSFW commands into one command. :)

--- commands/nsfw/nsfw.js
+++ commands/nsfw/nsfw.js
@@ -5,7 +5,7 @@ const {error_log} = require('../../config');
 const {errorMessage} = require('../../functions/logHandler');
 const ErrorEnum = require('../../functions/errorTypes')
 const { disgustP } = require('../../assets/json/actions.json');
-//const {suv}
+const {SubsToSearch} = require('./subs');
 
 function getRndInteger(min, max) {
     return Math.floor(Math.random() * (max - min) ) + min;
@@ -15,48 +15,62 @@ function getRndInteger(min, max) {
 
 module.exports = class NSFWCommand extends Command{
     constructor(client) {
+
+
         super(client, {
             name: 'nsfw',
             description: 'Search for NSFW Pics based on requested SubList',
             memberName: 'nsfw',
             group: 'nsfw',
+            guildOnly: true,
+            nsfw: true,
             aliases: ['nsfwpics','pornpics'],
             examples: ['!nsfw 4knsfw','!nsfw pussy'],
             args: [
                 {
                     key: 'subList',
                     type: 'string',
-                    prompt: 'Please enter the category to search'
+                    prompt: `Please enter the category to search\n**Options**: ${Object.keys(SubsToSearch)} `
                 }
             ]
+
         });
 
     }
     async run(message,{subList}){
 
 
-        await axios.get(`https://www.reddit.com/r/${subList[ getRndInteger(0,subList.length)]}.json`)
-            .then(function(res) {
 
-                for(var value in res.data.data.children){
-                    var index = getRndInteger(0,res.data.data.children.length);
-                    const embed = new Discord.MessageEmbed()
-                        .setFooter()
-                        .setDescription(`[Image URL](${res.data.data.children[getRndInteger(0,res.data.data.children.length)].data.permalink})`)
-                        .setImage(res.data.data.children[getRndInteger(0,res.data.data.children.length)].data.url)
-                        .setColor('#A187E0');
-                    return message.channel.send({ embed });
-
-                }
+        if(!message.guild.channels.cache.find(channel => channel.id == message.channel.id).nsfw ){
+            return message.say('This is **NOT** a NSFW Channel!!!!');
+        }
 
 
-            })
-            .catch(function (error) {
-                // handle error
 
-                message.client.channels.cache.get(error_log).send({embed: errorMessage(error,ErrorEnum.API,message.command.name)});
+            await axios.get(`https://www.reddit.com/r/${SubsToSearch[subList][getRndInteger(0,SubsToSearch[subList].length)]}.json`)
+                .then(function (res) {
 
-            });
+                    for (var value in res.data.data.children) {
+                        var index = getRndInteger(0, res.data.data.children.length);
+                        const embed = new Discord.MessageEmbed()
+                            .setFooter(`${subList}`)
+                            .setDescription(`[Image URL](${res.data.data.children[getRndInteger(0, res.data.data.children.length)].data.permalink})`)
+                            .setImage(res.data.data.children[getRndInteger(0, res.data.data.children.length)].data.url)
+                            .setColor('#A187E0');
+                        return message.channel.send({embed});
+
+                    }
+
+
+                })
+                .catch(function (error) {
+                    // handle error
+
+                    message.client.channels.cache.get(error_log).send({embed: errorMessage(error, ErrorEnum.API, message.command.name)});
+
+                });
+
+
 
     }
 
--- commands/nsfw/subs.js
+++ commands/nsfw/subs.js
@@ -1,4 +1,4 @@
-module.exports = SubsToSearch = {
+const SubsToSearch = {
     '4knsfw':
         [
             'nsfw_wallpapers',
@@ -7,7 +7,7 @@ module.exports = SubsToSearch = {
             'nsfw_hd',
             'UHDnsfw'
         ],
-    'ahegao':
+    "ahegao":
         [
          'ahegao'
         ],
@@ -18,14 +18,143 @@ module.exports = SubsToSearch = {
             'gonewild'
 
         ],
-    nsfw:
+
+    'nsfw':
         [
-            "nsfw",
-            "porn",
-            "BonerMaterial",
-            "adorableporn",
-            "nsfw2",
-            "Sexy",
-            "NSFW_nospam"
-        ]
+            'nsfw',
+            'porn',
+            'BonerMaterial',
+            'adorableporn',
+            'nsfw2',
+            'Sexy',
+            'NSFW_nospam'
+        ],
+    'artsyporn':
+        [
+            'SexyButNotPorn'
+        ],
+    'asian':
+        [
+            'AsianHotties',
+            'juicyasians',
+            'asianbabes'
+        ],
+    'bara':
+        [
+            'baramanga'
+        ],
+    'bdsm':
+        [
+            'bdsm',
+            'bondage'
+        ],
+    'bondage':
+        [
+            'hentaibondage'
+        ],
+    'cosplay':
+        [
+            'nsfwcosplay',
+            'cosplayonoff',
+            'cosporn',
+            'cosplayboobs'
+        ],
+    'ecchi':
+        [
+            'ecchi'
+        ],
+    'futa':
+        [
+            'futanari'
+        ],
+    'grool':
+        [
+            'grool'
+        ],
+    'hentai':
+        [
+            'hentai',
+            'rule34',
+            'HQHentai'
+        ],
+    'hentaigif':
+        [
+            'HENTAI_GIF'
+        ],
+    'hentaiirl':
+        [
+            'hentai_irl'
+        ],
+    'lingerie':
+        [
+            'lingerie',
+            'stockings',
+            'pantyfetish',
+            'panties'
+        ],
+    'monstergirl':
+        [
+            'MonsterGirl'
+        ],
+    'nsfwgif':
+        [
+            'NSFW_GIF',
+            'nsfw_gifs',
+            'porninfifteenseconds',
+            '60FPSPorn',
+            'porn_gifs',
+            'nsfw_Best_Porn_Gif',
+            'LipsThatGrip',
+            'adultgifs'
+        ],
+    'oppai':
+        [
+            'OppaiLove'
+        ],
+    'paizuri':
+        [
+            'Paizuri'
+        ],
+    'pantsu':
+        [
+            'pantsu'
+        ],
+    'pussy':
+        [
+            'pussy',
+            'rearpussy',
+            'simps',
+            'vagina',
+            'MoundofVenus',
+            'PerfectPussies',
+            'spreading'
+        ],
+    'sukebei':
+        [
+            'Sukebei'
+        ],
+    'tentacle':
+        [
+            'tentai'
+        ],
+    'trap':
+        [
+            'traphentai'
+        ],
+    'yaoi':
+        [
+            'yaoi'
+        ],
+    'yuri':
+        [
+            'yuri'
+        ],
+    'zr':
+        [
+            'ZettaiRyouiki'
+        ],
 };
+
+module.exports = {
+    SubsToSearch
+}
