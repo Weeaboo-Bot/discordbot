1ecb6eed2b4b96ca3b62082f49eb8ff44ba07956
Sean Doran <sdoran@iastate.edu>
2020-06-07 02:09:02 -0500

    sadadq

--- commands/music/play.js
+++ commands/music/play.js
@@ -36,7 +36,7 @@ module.exports = class PlayCommand extends Command {
 	async run(message, { query, client }) {
 
 		if (!message.channel.id === '713913929981755493') {
-			message.send('This command is not valid here!');
+			message.say('This command is not valid here!');
 		}
 		const voiceChannel = message.member.voice.channel;
 		if (!voiceChannel) return message.say('Join a channel and try again');
