1b5f2bca92bac008da4e6d8cd76de3cf1c3c0c18
Sean Doran <sdoran35@gmail.com>
2020-09-03 21:54:36 -0500

    oops

--- commands/games/blackjack.js
+++ commands/games/blackjack.js
@@ -37,9 +37,9 @@ module.exports = class BlackJackCommand extends Command {
 			else {
 				return Number(card);
 			}
-	
-		}
-	
+
+		};
+
 		const ace_check = (value, total) => {
 			if(total + value > 21 && value == 11) {
 				return 1;
@@ -47,46 +47,48 @@ module.exports = class BlackJackCommand extends Command {
 			else {
 				return value;
 			}
-		}
+		};
 
-		const arrCardCalc(arr){
-			var sum = 0;
-			var iterations = 0;
-			for(var x in arr){
+		const arrCardCalc = (arr) => {
+			let sum = 0;
+			let iterations = 0;
+			for(const x in arr) {
 				sum += arr[iterations].toInteger;
 				iterations += 1;
 			}
 			return [sum, iterations];
-		}
-	
-		const end_game = (user) => {
-			var userLsID = user.id + 'blackjackGame';
-			ls.remove(userLsID);
-		}
-	
-		const user_hit = (user, message, prefix) => {
-			var cardsObj = ls.getObj(user.id + 'blackjackGame');
-			var card = Deck.deal;
-			var total = arrCardCalc(cardsObj.userTotal);
-			var newTotal = total[0] + ace_check(card.to_integer, total[0]);
-			var userLS = user.id + 'profile';
-			var bet = Number(cardsObj.bet);
+		};
 
-			message.channel.send(`${user.username} drew ${card.val + "" + card.suite}`);
-			if(newTotal > 21){
-				var loss = Number(ls.get(userLS)) - (bet);
+		const end_game = (user) => {
+			const userLsID = user.id + 'blackjackGame';
+			ls.remove(userLsID);
+		};
+
+		const user_hit = (user, message, prefix) => {
+			const cardsObj = ls.getObj(user.id + 'blackjackGame');
+			const card = Deck.deal;
+			const total = arrCardCalc(cardsObj.userTotal);
+			const newTotal = total[0] + ace_check(card.to_integer, total[0]);
+			const userLS = user.id + 'profile';
+			const bet = Number(cardsObj.bet);
+
+			message.channel.send(`${user.username} drew ${card.val + '' + card.suite}`);
+			if(newTotal > 21) {
+				const loss = Number(ls.get(userLS)) - (bet);
 				ls.set(userLS, loss);
 				end_game(user);
 				return `${user.username} busted; Dealer Winds, **${user.username} loses`;
-			} else if(newTotal == 21 && total[1] == 1){
-				var gain = Number(ls.get(userLS)) + (bet * 2);
+			}
+			else if(newTotal == 21 && total[1] == 1) {
+				const gain = Number(ls.get(userLS)) + (bet * 2);
 				ls.set(userLS, gain);
 				end_game(user);
 				return `${user.username} has blackjack; **${user.username} wins!, doubled the winning ammount**!`;
-			} else {
-				cardsObj.userTotal.push()
 			}
-		}
+			else {
+				cardsObj.userTotal.push();
+			}
+		};
 
 
 		const deckEmbed = new Discord.MessageEmbed()
@@ -105,7 +107,6 @@ module.exports = class BlackJackCommand extends Command {
 
 
 	}
-
 
 
 };
