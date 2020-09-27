6bfb395a81814ac8efb61fa68892198110f97648
Restyled.io <commits@restyled.io>
2020-09-27 03:33:43 +0000

    Restyled by prettier

--- commands/utility/compvision.js
+++ commands/utility/compvision.js
@@ -1,17 +1,17 @@
-const Discord = require('discord.js');
-const Command = require('../../models/Command');
+const Discord = require("discord.js");
+const Command = require("../../models/Command");
 
 module.exports = class CompVisionCommand extends Command {
   constructor(client) {
     super(client, {
-      name : 'compvision',
-      group : 'utility',
-      memberName : 'compvision',
-      guildOnly : true,
-      aliases : [ 'compvis', 'recg' ],
-      description : 'Use Computer Vision to analyze an image'
+      name: "compvision",
+      group: "utility",
+      memberName: "compvision",
+      guildOnly: true,
+      aliases: ["compvis", "recg"],
+      description: "Use Computer Vision to analyze an image",
     });
   }
 
   async run(message) {}
-}
+};
