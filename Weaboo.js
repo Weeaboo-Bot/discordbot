3f9f28d60033142373c19c2bbb975282f1e63f50
Restyled.io <commits@restyled.io>
2020-09-27 03:33:32 +0000

    Restyled by clang-format

--- Weaboo.js
+++ Weaboo.js
@@ -3,129 +3,115 @@
 require('./helpers/extenders');
 const clipboard = require('clipboardy');
 const Client = require('./models/WeabooClient');
-const util = require('util'),
-	fs = require('fs'),
-	path = require('path'),
-	readdir = util.promisify(fs.readdir),
-	mongoose = require('mongoose'),
-	logger = require('discordjs-logger'),
-	custom_log = require('./helpers/logger');
+const util = require('util'), fs = require('fs'), path = require('path'),
+      readdir = util.promisify(fs.readdir), mongoose = require('mongoose'),
+      logger = require('discordjs-logger'),
+      custom_log = require('./helpers/logger');
 
 const config = require('./config');
-const { formatNumber } = require('./helpers/functions');
-const { Intents, MessageEmbed } = require('discord.js');
-
+const {formatNumber} = require('./helpers/functions');
+const {Intents, MessageEmbed} = require('discord.js');
 
 const client = new Client({
-	partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
-	commandPrefix: '%',
-	owner: config.owner.id,
-	disableEveryone: true,
-	unknownCommandResponse: false,
-	ws: { intents: Intents.ALL },
-	// messageCacheMaxSize	= 50,
+  partials : [ 'MESSAGE', 'CHANNEL', 'REACTION' ],
+  commandPrefix : '%',
+  owner : config.owner.id,
+  disableEveryone : true,
+  unknownCommandResponse : false,
+  ws : {intents : Intents.ALL},
+  // messageCacheMaxSize	= 50,
 });
 
-
 client.registry
-	.registerDefaultTypes()
+    .registerDefaultTypes()
 
-	.registerGroups([
-		['action', 'Action'],
-		['anime', 'Anime'],
-		['music', 'Music'],
-		['fun', 'Fun'],
-		['core', 'Core'],
-		['info', 'Info'],
-		['memes', 'Memes'],
-		['moderation', 'Moe-Deration'],
-		['nsfw', 'NSFW'],
-		['utility', 'Utility'],
-		['owner', 'Hidden + Owner'],
-		['news', 'News'],
-		['general', 'General'],
-		['games', 'Games'],
-		['video', 'Video Commands'],
-		['loyal', 'Loyalty Program Commands'],
-		['number', 'Number Commands'],
-	])
-	.registerDefaultGroups()
+    .registerGroups([
+      [ 'action', 'Action' ],
+      [ 'anime', 'Anime' ],
+      [ 'music', 'Music' ],
+      [ 'fun', 'Fun' ],
+      [ 'core', 'Core' ],
+      [ 'info', 'Info' ],
+      [ 'memes', 'Memes' ],
+      [ 'moderation', 'Moe-Deration' ],
+      [ 'nsfw', 'NSFW' ],
+      [ 'utility', 'Utility' ],
+      [ 'owner', 'Hidden + Owner' ],
+      [ 'news', 'News' ],
+      [ 'general', 'General' ],
+      [ 'games', 'Games' ],
+      [ 'video', 'Video Commands' ],
+      [ 'loyal', 'Loyalty Program Commands' ],
+      [ 'number', 'Number Commands' ],
+    ])
+    .registerDefaultGroups()
 
-	.registerDefaultCommands({
-		eval: false,
-		prefix: false,
-		commandState: false,
-		ping: false,
-		commands: false,
-		webhook: false,
+    .registerDefaultCommands({
+      eval : false,
+      prefix : false,
+      commandState : false,
+      ping : false,
+      commands : false,
+      webhook : false,
 
-
-	})
-	.registerCommandsIn(path.join(__dirname, 'commands'));
-
+    })
+    .registerCommandsIn(path.join(__dirname, 'commands'));
 
 const dbConnect = () => {
-	// connect to mongoose database
-	mongoose.connect(config.mongoDB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-		console.log('Connected to the Mongodb database.', 'log');
-	}).catch((err) => {
-		console.log('Unable to connect to the Mongodb database. Error:' + err, 'error');
-	});
+  // connect to mongoose database
+  mongoose
+      .connect(config.mongoDB,
+               {useNewUrlParser : true, useUnifiedTopology : true})
+      .then(() => { console.log('Connected to the Mongodb database.', 'log'); })
+      .catch((err) => {
+        console.log('Unable to connect to the Mongodb database. Error:' + err,
+                    'error');
+      });
 };
 
-
 const init = async (res) => {
+  // Search for all commands
+  const directories = await readdir('./commands/');
+  custom_log.log(`Loading a total of ${directories.length} categories.`,
+                 'success');
+  for (const dir of directories) {
+    const commands = await readdir('./commands/' + dir + '/');
+    custom_log.log(`Loading a total of ${commands.length} commands in ${dir}`,
+                   'success');
+    commands.filter((cmd) => cmd.split('.').pop() === 'js').forEach((cmd) => {
+      custom_log.log(`Loading Command: ${cmd}`, 'output');
+    });
+  }
 
-	// Search for all commands
-	const directories = await readdir('./commands/');
-	custom_log.log(`Loading a total of ${directories.length} categories.`, 'success');
-	for (const dir of directories) {
-		const commands = await readdir('./commands/' + dir + '/');
-		custom_log.log(`Loading a total of ${commands.length} commands in ${dir}`, 'success');
-		commands.filter((cmd) => cmd.split('.').pop() === 'js').forEach((cmd) => {
-
-			custom_log.log(`Loading Command: ${cmd}`, 'output');
-
-		});
-	}
-
-
-	// Then we load events, which will include our message and ready event.
-	const evtFiles = await readdir('./events/');
-	custom_log.log(`Loading a total of ${evtFiles.length} events.`, 'success');
-	evtFiles.forEach((file) => {
-		const eventName = file.split('.')[0];
-		custom_log.log(`Loading Event: ${eventName}`, 'output');
-
-	});
-	// const languages = require("./helpers/languages");
-	// client.translations = await languages();
-	dbConnect();
-
-
+  // Then we load events, which will include our message and ready event.
+  const evtFiles = await readdir('./events/');
+  custom_log.log(`Loading a total of ${evtFiles.length} events.`, 'success');
+  evtFiles.forEach((file) => {
+    const eventName = file.split('.')[0];
+    custom_log.log(`Loading Event: ${eventName}`, 'output');
+  });
+  // const languages = require("./helpers/languages");
+  // client.translations = await languages();
+  dbConnect();
 };
 
 client.on('ready', () => {
+  init();
 
-
-	init();
-
-	console.log(`[READY] Logged in as ${client.user.tag}! ID: ${client.user.id}`);
-	custom_log.log('hello', 'twitch');
+  console.log(`[READY] Logged in as ${client.user.tag}! ID: ${client.user.id}`);
+  custom_log.log('hello', 'twitch');
 });
-
 
 // if there are errors, log them
-client.on('disconnect', () => client.logger.log('Bot is disconnecting...', 'warn'))
-	.on('reconnecting', () => client.logger.log('Bot reconnecting...', 'log'))
-	.on('error', (e) => client.logger.log(e, 'error'))
-	.on('warn', (info) => client.logger.log(info, 'warn'));
+client
+    .on('disconnect',
+        () => client.logger.log('Bot is disconnecting...', 'warn'))
+    .on('reconnecting', () => client.logger.log('Bot reconnecting...', 'log'))
+    .on('error', (e) => client.logger.log(e, 'error'))
+    .on('warn', (info) => client.logger.log(info, 'warn'));
 
 // if there is an unhandledRejection, log them
-process.on('unhandledRejection', (err) => {
-	console.error(err);
-});
+process.on('unhandledRejection', (err) => { console.error(err); });
 
 // logger.all(client);
 client.login(config.token);
-
--- models/WeabooClient.js
+++ models/WeabooClient.js
@@ -1,9 +1,7 @@
-const { CommandoClient } = require('discord.js-commando');
-const { Collection, MessageEmbed, Structures } = require('discord.js');
-const util = require('util'),
-	path = require('path'),
-	fs = require('fs'),
-	moment = require('moment');
+const {CommandoClient} = require('discord.js-commando');
+const {Collection, MessageEmbed, Structures} = require('discord.js');
+const util = require('util'), path = require('path'), fs = require('fs'),
+      moment = require('moment');
 
 moment.relativeTimeThreshold('s', 60);
 moment.relativeTimeThreshold('ss', 5);
@@ -13,240 +11,271 @@ moment.relativeTimeThreshold('d', 24);
 moment.relativeTimeThreshold('M', 12);
 
 Structures.extend('Guild', function(Guild) {
-	class MusicGuild extends Guild {
-		constructor(client, data) {
-			super(client, data);
-			this.musicData = {
-				queue: [],
-				isPlaying: false,
-				nowPlaying: null,
-				songDispatcher: null,
-				volume: 1,
-			};
-
-		}
-	}
-	return MusicGuild;
+  class MusicGuild extends Guild {
+    constructor(client, data) {
+      super(client, data);
+      this.musicData = {
+        queue : [],
+        isPlaying : false,
+        nowPlaying : null,
+        songDispatcher : null,
+        volume : 1,
+      };
+    }
+  }
+  return MusicGuild;
 });
 
-
 module.exports = class Client extends CommandoClient {
-	constructor(options) {
-		super(options);
-		this.logger = require('../helpers/logger');
-		this.meme = 'hello';
-		this.config = require('../config'); // Load the config file
-		this.customEmojis = require('../emojis.json'); // load the bot's emojis
-		this.wait = util.promisify(setTimeout); // client.wait(1000) - Wait 1 second
-		this.functions = require('../helpers/functions'); // Load the functions file
-		this.guildsData = require('../models/Guild'); // Guild mongoose model
-		this.usersData = require('../models/User'); // User mongoose model
-		this.membersData = require('../models/Member'); // Member mongoose model
-		this.logs = require('../models/Log'); // Log mongoose model
-		this.games = new Collection();
-		this.databaseCache = {};
-		this.databaseCache.users = new Collection();
-		this.databaseCache.guilds = new Collection();
-		this.databaseCache.members = new Collection();
+  constructor(options) {
+    super(options);
+    this.logger = require('../helpers/logger');
+    this.meme = 'hello';
+    this.config = require('../config');            // Load the config file
+    this.customEmojis = require('../emojis.json'); // load the bot's emojis
+    this.wait = util.promisify(setTimeout); // client.wait(1000) - Wait 1 second
+    this.functions = require('../helpers/functions'); // Load the functions file
+    this.guildsData = require('../models/Guild');     // Guild mongoose model
+    this.usersData = require('../models/User');       // User mongoose model
+    this.membersData = require('../models/Member');   // Member mongoose model
+    this.logs = require('../models/Log');             // Log mongoose model
+    this.games = new Collection();
+    this.databaseCache = {};
+    this.databaseCache.users = new Collection();
+    this.databaseCache.guilds = new Collection();
+    this.databaseCache.members = new Collection();
 
-		this.databaseCache.usersReminds = new Collection(); // members with active reminds
-		this.databaseCache.mutedUsers = new Collection(); // members who are currently muted
+    this.databaseCache.usersReminds =
+        new Collection(); // members with active reminds
+    this.databaseCache.mutedUsers =
+        new Collection(); // members who are currently muted
+  }
 
+  onError(err, message, args, fromPattern,
+          result) { // eslint-disable-line no-unused-vars
+    console.error(err);
+    const embed =
+        new MessageEmbed()
+            .setColor('RED')
+            .setTimestamp()
+            .setTitle(
+                'Please report this on our Discord server https://lenoxbot.com/discord')
+            .setDescription(`StackTrace: \n\`\`\`${err.stack}\`\`\``)
+            .addField('Command:', `${message.content.split(' ').join(' ')}`);
 
-	}
+    return message.reply({embed});
+  }
 
-	onError(err, message, args, fromPattern, result) { // eslint-disable-line no-unused-vars
-		console.error(err);
-		const embed = new MessageEmbed()
-			.setColor('RED')
-			.setTimestamp()
-			.setTitle('Please report this on our Discord server https://lenoxbot.com/discord')
-			.setDescription(`StackTrace: \n\`\`\`${err.stack}\`\`\``)
-			.addField('Command:', `${message.content.split(' ').join(' ')}`);
+  get defaultLanguage() {
+    return this.config.languages.find((language) => language.default).name;
+  }
 
-		return message.reply({ embed });
-	}
+  translate(key, args, locale) {
+    if (!locale)
+      locale = this.defaultLanguage;
+    const language = this.translations.get(locale);
+    if (!language)
+      throw 'Invalid language set in data.';
+    return language(key, args);
+  }
 
-	get defaultLanguage() {
-		return this.config.languages.find((language) => language.default).name;
-	}
+  printDate(date, format, locale) {
+    if (!locale)
+      locale = this.defaultLanguage;
+    const languageData = this.config.languages.find(
+        (language) =>
+            language.name === locale || language.aliases.includes(locale));
+    if (!format)
+      format = languageData.defaultMomentFormat;
+    return moment(new Date(date)).locale(languageData.moment).format(format);
+  }
 
-	translate(key, args, locale) {
-		if(!locale) locale = this.defaultLanguage;
-		const language = this.translations.get(locale);
-		if (!language) throw 'Invalid language set in data.';
-		return language(key, args);
-	}
+  convertTime(time, type, noPrefix, locale) {
+    if (!type)
+      time = 'to';
+    if (!locale)
+      locale = this.defaultLanguage;
+    const languageData = this.config.languages.find(
+        (language) =>
+            language.name === locale || language.aliases.includes(locale));
+    const m = moment(time).locale(languageData.moment);
+    return (type === 'to' ? m.toNow(noPrefix) : m.fromNow(noPrefix));
+  }
 
-	printDate(date, format, locale) {
-		if(!locale) locale = this.defaultLanguage;
-		const languageData = this.config.languages.find((language) => language.name === locale || language.aliases.includes(locale));
-		if(!format) format = languageData.defaultMomentFormat;
-		return moment(new Date(date))
-			.locale(languageData.moment)
-			.format(format);
-	}
+  get defaultLanguage() {
+    return this.config.languages.find((language) => language.default).name;
+  }
 
-	convertTime(time, type, noPrefix, locale) {
-		if(!type) time = 'to';
-		if(!locale) locale = this.defaultLanguage;
-		const languageData = this.config.languages.find((language) => language.name === locale || language.aliases.includes(locale));
-		const m = moment(time)
-			.locale(languageData.moment);
-		return (type === 'to' ? m.toNow(noPrefix) : m.fromNow(noPrefix));
-	}
+  translate(key, args, locale) {
+    if (!locale)
+      locale = this.defaultLanguage;
+    const language = this.translations.get(locale);
+    if (!language)
+      throw 'Invalid language set in data.';
+    return language(key, args);
+  }
 
-	get defaultLanguage() {
-		return this.config.languages.find((language) => language.default).name;
-	}
+  printDate(date, format, locale) {
+    if (!locale)
+      locale = this.defaultLanguage;
+    const languageData = this.config.languages.find(
+        (language) =>
+            language.name === locale || language.aliases.includes(locale));
+    if (!format)
+      format = languageData.defaultMomentFormat;
+    return moment(new Date(date)).locale(languageData.moment).format(format);
+  }
 
-	translate(key, args, locale) {
-		if(!locale) locale = this.defaultLanguage;
-		const language = this.translations.get(locale);
-		if (!language) throw 'Invalid language set in data.';
-		return language(key, args);
-	}
+  convertTime(time, type, noPrefix, locale) {
+    if (!type)
+      time = 'to';
+    if (!locale)
+      locale = this.defaultLanguage;
+    const languageData = this.config.languages.find(
+        (language) =>
+            language.name === locale || language.aliases.includes(locale));
+    const m = moment(time).locale(languageData.moment);
+    return (type === 'to' ? m.toNow(noPrefix) : m.fromNow(noPrefix));
+  }
 
-	printDate(date, format, locale) {
-		if(!locale) locale = this.defaultLanguage;
-		const languageData = this.config.languages.find((language) => language.name === locale || language.aliases.includes(locale));
-		if(!format) format = languageData.defaultMomentFormat;
-		return moment(new Date(date))
-			.locale(languageData.moment)
-			.format(format);
-	}
+  // This function is used to find a user data or create it
+  async findOrCreateUser({id : userID}, isLean) {
+    if (this.databaseCache.users.get(userID)) {
+      return isLean ? this.databaseCache.users.get(userID).toJSON()
+                    : this.databaseCache.users.get(userID);
+    } else {
+      let userData =
+          (isLean ? await this.usersData.findOne({id : userID}).lean()
+                  : await this.usersData.findOne({id : userID}));
+      if (userData) {
+        if (!isLean)
+          this.databaseCache.users.set(userID, userData);
+        return userData;
+      } else {
+        userData = new this.usersData({id : userID});
+        await userData.save();
+        this.databaseCache.users.set(userID, userData);
+        return isLean ? userData.toJSON() : userData;
+      }
+    }
+  }
 
-	convertTime(time, type, noPrefix, locale) {
-		if(!type) time = 'to';
-		if(!locale) locale = this.defaultLanguage;
-		const languageData = this.config.languages.find((language) => language.name === locale || language.aliases.includes(locale));
-		const m = moment(time)
-			.locale(languageData.moment);
-		return (type === 'to' ? m.toNow(noPrefix) : m.fromNow(noPrefix));
-	}
+  // This function is used to find a member data or create it
+  async findOrCreateMember({id : memberID, guildID}, isLean) {
+    if (this.databaseCache.members.get(`${memberID}${guildID}`)) {
+      return isLean ? this.databaseCache.members.get(`${memberID}${guildID}`)
+                          .toJSON()
+                    : this.databaseCache.members.get(`${memberID}${guildID}`);
+    } else {
+      let memberData =
+          (isLean
+               ? await this.membersData.findOne({guildID, id : memberID}).lean()
+               : await this.membersData.findOne({guildID, id : memberID}));
+      if (memberData) {
+        if (!isLean)
+          this.databaseCache.members.set(`${memberID}${guildID}`, memberData);
+        return memberData;
+      } else {
+        memberData = new this.membersData({id : memberID, guildID : guildID});
+        await memberData.save();
+        const guild = await this.findOrCreateGuild({id : guildID});
+        if (guild) {
+          guild.members.push(memberData._id);
+          await guild.save();
+        }
+        this.databaseCache.members.set(`${memberID}${guildID}`, memberData);
+        return isLean ? memberData.toJSON() : memberData;
+      }
+    }
+  }
 
+  // This function is used to find a guild data or create it
+  async findOrCreateGuild({id : guildID}, isLean) {
+    if (this.databaseCache.guilds.get(guildID)) {
+      return isLean ? this.databaseCache.guilds.get(guildID).toJSON()
+                    : this.databaseCache.guilds.get(guildID);
+    } else {
+      let guildData = (isLean ? await this.guildsData.findOne({id : guildID})
+                                    .populate('members')
+                                    .lean()
+                              : await this.guildsData.findOne({id : guildID})
+                                    .populate('members'));
+      if (guildData) {
+        if (!isLean)
+          this.databaseCache.guilds.set(guildID, guildData);
+        return guildData;
+      } else {
+        guildData = new this.guildsData({id : guildID});
+        await guildData.save();
+        this.databaseCache.guilds.set(guildID, guildData);
+        return isLean ? guildData.toJSON() : guildData;
+      }
+    }
+  }
 
-	// This function is used to find a user data or create it
-	async findOrCreateUser({ id: userID }, isLean) {
-		if(this.databaseCache.users.get(userID)) {
-			return isLean ? this.databaseCache.users.get(userID).toJSON() : this.databaseCache.users.get(userID);
-		}
-		else {
-			let userData = (isLean ? await this.usersData.findOne({ id: userID }).lean() : await this.usersData.findOne({ id: userID }));
-			if(userData) {
-				if(!isLean) this.databaseCache.users.set(userID, userData);
-				return userData;
-			}
-			else {
-				userData = new this.usersData({ id: userID });
-				await userData.save();
-				this.databaseCache.users.set(userID, userData);
-				return isLean ? userData.toJSON() : userData;
-			}
-		}
-	}
+  // This function is used to resolve a user from a string
+  async resolveUser(search) {
+    let user = null;
+    if (!search || typeof search !== 'string')
+      return;
+    // Try ID search
+    if (search.match(/^<@!?(\d+)>$/)) {
+      const id = search.match(/^<@!?(\d+)>$/)[1];
+      user = this.users.fetch(id).catch(() => {});
+      if (user)
+        return user;
+    }
+    // Try username search
+    if (search.match(/^!?(\w+)#(\d+)$/)) {
+      const username = search.match(/^!?(\w+)#(\d+)$/)[0];
+      const discriminator = search.match(/^!?(\w+)#(\d+)$/)[1];
+      user = this.users.find((u) => u.username === username &&
+                                    u.discriminator === discriminator);
+      if (user)
+        return user;
+    }
+    user = await this.users.fetch(search).catch(() => {});
+    return user;
+  }
 
-	// This function is used to find a member data or create it
-	async findOrCreateMember({ id: memberID, guildID }, isLean) {
-		if(this.databaseCache.members.get(`${memberID}${guildID}`)) {
-			return isLean ? this.databaseCache.members.get(`${memberID}${guildID}`).toJSON() : this.databaseCache.members.get(`${memberID}${guildID}`);
-		}
-		else {
-			let memberData = (isLean ? await this.membersData.findOne({ guildID, id: memberID }).lean() : await this.membersData.findOne({ guildID, id: memberID }));
-			if(memberData) {
-				if(!isLean) this.databaseCache.members.set(`${memberID}${guildID}`, memberData);
-				return memberData;
-			}
-			else {
-				memberData = new this.membersData({ id: memberID, guildID: guildID });
-				await memberData.save();
-				const guild = await this.findOrCreateGuild({ id: guildID });
-				if(guild) {
-					guild.members.push(memberData._id);
-					await guild.save();
-				}
-				this.databaseCache.members.set(`${memberID}${guildID}`, memberData);
-				return isLean ? memberData.toJSON() : memberData;
-			}
-		}
-	}
+  async resolveMember(search, guild) {
+    let member = null;
+    if (!search || typeof search !== 'string')
+      return;
+    // Try ID search
+    if (search.match(/^<@!?(\d+)>$/)) {
+      const id = search.match(/^<@!?(\d+)>$/)[1];
+      member = await guild.members.fetch(id).catch(() => {});
+      if (member)
+        return member;
+    }
+    // Try username search
+    if (search.match(/^!?(\w+)#(\d+)$/)) {
+      guild = await guild.fetch();
+      member = guild.members.cache.find((m) => m.user.tag === search);
+      if (member)
+        return member;
+    }
+    member = await guild.members.fetch(search).catch(() => {});
+    return member;
+  }
 
-	// This function is used to find a guild data or create it
-	async findOrCreateGuild({ id: guildID }, isLean) {
-		if(this.databaseCache.guilds.get(guildID)) {
-			return isLean ? this.databaseCache.guilds.get(guildID).toJSON() : this.databaseCache.guilds.get(guildID);
-		}
-		else {
-			let guildData = (isLean ? await this.guildsData.findOne({ id: guildID }).populate('members').lean() : await this.guildsData.findOne({ id: guildID }).populate('members'));
-			if(guildData) {
-				if(!isLean) this.databaseCache.guilds.set(guildID, guildData);
-				return guildData;
-			}
-			else {
-				guildData = new this.guildsData({ id: guildID });
-				await guildData.save();
-				this.databaseCache.guilds.set(guildID, guildData);
-				return isLean ? guildData.toJSON() : guildData;
-			}
-		}
-	}
-
-
-	// This function is used to resolve a user from a string
-	async resolveUser(search) {
-		let user = null;
-		if(!search || typeof search !== 'string') return;
-		// Try ID search
-		if(search.match(/^<@!?(\d+)>$/)) {
-			const id = search.match(/^<@!?(\d+)>$/)[1];
-			user = this.users.fetch(id).catch(() => {});
-			if(user) return user;
-		}
-		// Try username search
-		if(search.match(/^!?(\w+)#(\d+)$/)) {
-			const username = search.match(/^!?(\w+)#(\d+)$/)[0];
-			const discriminator = search.match(/^!?(\w+)#(\d+)$/)[1];
-			user = this.users.find((u) => u.username === username && u.discriminator === discriminator);
-			if(user) return user;
-		}
-		user = await this.users.fetch(search).catch(() => {});
-		return user;
-	}
-
-	async resolveMember(search, guild) {
-		let member = null;
-		if(!search || typeof search !== 'string') return;
-		// Try ID search
-		if(search.match(/^<@!?(\d+)>$/)) {
-			const id = search.match(/^<@!?(\d+)>$/)[1];
-			member = await guild.members.fetch(id).catch(() => {});
-			if(member) return member;
-		}
-		// Try username search
-		if(search.match(/^!?(\w+)#(\d+)$/)) {
-			guild = await guild.fetch();
-			member = guild.members.cache.find((m) => m.user.tag === search);
-			if(member) return member;
-		}
-		member = await guild.members.fetch(search).catch(() => {});
-		return member;
-	}
-
-	async resolveRole(search, guild) {
-		let role = null;
-		if(!search || typeof search !== 'string') return;
-		// Try ID search
-		if(search.match(/^<@&!?(\d+)>$/)) {
-			const id = search.match(/^<@&!?(\d+)>$/)[1];
-			role = guild.roles.cache.get(id);
-			if(role) return role;
-		}
-		// Try name search
-		role = guild.roles.cache.find((r) => search === r.name);
-		if(role) return role;
-		role = guild.roles.cache.get(search);
-		return role;
-	}
-
-
+  async resolveRole(search, guild) {
+    let role = null;
+    if (!search || typeof search !== 'string')
+      return;
+    // Try ID search
+    if (search.match(/^<@&!?(\d+)>$/)) {
+      const id = search.match(/^<@&!?(\d+)>$/)[1];
+      role = guild.roles.cache.get(id);
+      if (role)
+        return role;
+    }
+    // Try name search
+    role = guild.roles.cache.find((r) => search === r.name);
+    if (role)
+      return role;
+    role = guild.roles.cache.get(search);
+    return role;
+  }
 };
