f0cf8ae58636ca7268a885a9c3b4cab5f57547b5
Sean Doran <sdoran35@gmail.com>
2021-01-04 19:57:13 -0600

    pem file

--- config.js
+++ config.js
@@ -60,7 +60,7 @@ exports.fbAdminConfig = {
 	'type': process.env.ADMIN_TYPE,
 	'project_id': process.env.ADMIN_PROJECT_ID,
 	'private_key_id': process.env.ADMIN_PRIVATE_KEY_ID,
-	'private_key': process.env.ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'),
+	'private_key': process.env.ADMIN_PRIVATE_KEY,
 	'client_email': process.env.ADMIN_CLIENT_EMAIL,
 	'client_id': process.env.ADMIN_CLIENT_ID,
 	'auth_uri': process.env.ADMIN_AUTH_URI,
