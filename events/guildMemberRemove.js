480f455ec20521a6a35d6829a806174977c212e5
Sean Doran <sdoran35@gmail.com>
2021-01-10 17:57:30 -0600

    Fixed play music command
    Signed-off-by: Sean Doran <sdoran35@gmail.com>

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,16 +1,25 @@
-const Database = require('../util/db');
-module.exports = async (client, guild, member) => {
+module.exports = async (client, member) => {
+	
+	client.logger.info(
+			`${member.guild.name}: ${member.user.tag} has left the server`);
 	if (member.id === client.user.id) return null;
 	if (member.partial) await member.fetch();
-	const channel = member.guild.systemChannel;
-	if (!channel || !channel.permissionsFor(client.user).has('SEND_MESSAGES')) return null;
-	if (channel.topic && channel.topic.includes('<weaboo:disable-leave>')) return null;
-	try {
-		const leaveMessage = client.leaveMessages[Math.floor(Math.random() * client.leaveMessages.length)];
-		await channel.send(leaveMessage.replaceAll('{{user}}', `**${member.user.tag}**`));
+	const channel = client.fetchJoinLeaveChannel();
+	if (!channel ||
+			!channel.permissionsFor(client.user).has('SEND_MESSAGES')) {
 		return null;
 	}
-	catch {
+	if (channel.topic &&
+			channel.topic.includes('<weaboo:disable-leave>')) {
+		return null;
+	}
+	try {
+		const leaveMessage = client.leaveMessages[Math.floor(
+				Math.random() * client.leaveMessages.length)];
+		await channel.send(
+				leaveMessage.replaceAll('{{user}}', `**${member.user.tag}**`));
+		return null;
+	} catch {
 		return null;
 	}
 };
--- events/messageDelete.js
+++ events/messageDelete.js
@@ -10,42 +10,47 @@ module.exports = async (client, message) => {
 	}).then(res => {
 		deletionLog = res.entries.first();
 	});
-
+	
 	// Let's perform a sanity check here and make sure we got *something*
-	if (!deletionLog) return console.log(`A message by ${message.author.tag} was deleted, but no relevant audit logs were found.`);
-
+	if (!deletionLog) {
+		return client.logger.info(
+				`A message by ${message.author.tag} was deleted, but no relevant audit logs were found.`);
+	}
+	
 	// We now grab the user object of the person who deleted the message
 	// Let us also grab the target of this action to double check things
 	const { executor, target } = deletionLog;
-
-
+	
+	
 	// And now we can update our output with a bit more information
 	// We will also run a check to make sure the log we got was for the same author's message
 	if (target.id === message.author.id) {
-
+		
 		const channel = client.channels.cache.get(client.auditLog);
 		const embed = new MessageEmbed()
-			.setTitle('Audit Event')
-			.setColor('#727293')
-			.addField('Audit Event Name', 'Message Deleted')
-			.addField('Member', message.author.tag)
-			.addField('Delete Event', executor.tag)
-			.setFooter(`v${version}`)
-			.setTimestamp();
+				.setTitle('Audit Event')
+				.setColor('#727293')
+				.addField('Audit Event Name', 'Message Deleted')
+				.addField('Member', message.author.tag)
+				.addField('Delete Event', executor.tag)
+				.setFooter(`v${version}`)
+				.setTimestamp();
 		channel.send({ embed });
-		console.log(`A message by ${message.author.tag} was deleted by ${executor.tag}.`);
+		client.logger.info(
+				`A message by ${message.author.tag} was deleted by ${executor.tag}.`);
 	}
 	else {
 		const channel = client.channels.cache.get(client.auditLog);
 		const embed = new MessageEmbed()
-			.setTitle('Audit Event')
-			.setColor('#727293')
-			.addField('Audit Event Name', 'Message Deleted')
-			.addField('Member', 'Member is Unkown')
-			.addField('Delete Event', 'Member is Unkown')
-			.setFooter(`v${version}`)
-			.setTimestamp();
+				.setTitle('Audit Event')
+				.setColor('#727293')
+				.addField('Audit Event Name', 'Message Deleted')
+				.addField('Member', 'Member is Unknown')
+				.addField('Delete Event', 'Member is Unknown')
+				.setFooter(`v${version}`)
+				.setTimestamp();
 		channel.send({ embed });
-		console.log(`A message by ${message.author.tag} was deleted, but we don't know by who.`);
+		client.logger.info(
+				`A message by ${message.author.tag} was deleted, but we don't know by who.`);
 	}
 };
