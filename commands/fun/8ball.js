8974461c3214a200f6d4a10b3cd2cce5980273eb
Snoo <wm28744@gmail.com>
2021-02-15 14:13:30 -0600

    New 8-ball functionality.
    Added gifs to go along with responses. Also moved the messages into 8-ball.json.

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -1,27 +1,6 @@
 const Command = require('../../structures/Command');
 const Discord = require('discord.js');
-const answer = [
-	'It is certain',
-	'It is decidedly so',
-	'Without a doubt',
-	'Yes, definitely',
-	'You may rely on it',
-	'As I see it, yes',
-	'Most likely',
-	'Yes',
-	'Signs point to yes',
-	'The reply is hazy, try again',
-	'Ask again later',
-	'I\'d better not tell you now',
-	'I cannot predict now',
-	'Concentrate and ask again',
-	'Don\'t count on it',
-	'My sources say no',
-	'The outlook isn\'t so good',
-	'Very doubtful',
-	'B-Baka! No!',
-	'Yes daddy...',
-];
+const { yesQuotes, laterQuotes, noQuotes, yesP, noP } = require('../../assets/json/8-ball.json');
 
 
 module.exports = class EightBallCommand extends Command {
@@ -43,15 +22,54 @@ module.exports = class EightBallCommand extends Command {
 
 	run(message) {
 		const question = message.content.split(/\s+/g).slice(1).join(' ');
+		const yes = yesQuotes[Math.round(Math.random() * (yesQuotes.length - 1))] + '.';
+		const later = laterQuotes[Math.round(Math.random() * (laterQuotes.length - 1))] + '.';
+		const no = noQuotes[Math.round(Math.random() * (noQuotes.length - 1))] + '.';
+		const yesgif = yesP[Math.round(Math.random() * (yesP.length - 1))];
+		const nogif = noP[Math.round(Math.random() * (noP.length - 1))];
+
 
 		if (!question) {
 			return message.channel.send('You must provide a question!');
 		}
 
-		const embed = new Discord.MessageEmbed()
-			.setAuthor(question, 'https://a.safe.moe/aKDHV.png')
-			.setDescription(answer[Math.round(Math.random() * (answer.length - 1))] + '.')
-			.setColor('#646770');
+		function randomNumber(min, max){
+			min = Math.ceil(min);
+			max = Math.floor(max);
+			return Math.floor(Math.random() * (max - min + 1)) + min;
+		}
+
+		//chooses a random number to determine which type of message + corresponding gif will be sent
+		var choice = randomNumber(1, 3);
+
+		//yes messsages and gifs
+		if(choice == 1){
+			const embed = new Discord.MessageEmbed()
+				.setAuthor(question)
+				.setDesciption(yes)
+				.setImage(yesgif)
+				.setColor('#646770')
+		}
+		//no messages and gifs
+		else if(choice == 2){
+			const embed = new Discord.MessageEmbed()
+				.setAuthor(question)
+				.setDesciption(no)
+				.setImage(nogif)
+				.setColor('#646770')
+		}
+		//later messages
+		else{
+			const embed = new Discord.MessageEmbed()
+				.setAuthor(question)
+				.setDesciption(later)
+				.setColor('#646770')
+		}
+			
+		//const embed = new Discord.MessageEmbed()
+		//	.setAuthor(question, 'https://a.safe.moe/aKDHV.png')
+		//	.setDescription(answer[Math.round(Math.random() * (answer.length - 1))] + '.')
+		//	.setColor('#646770');
 		return message.channel.send({ embed });
 
 	}
