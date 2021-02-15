fe34182512722973b24a9b7a4e05baa958930578
Snoo <wm28744@gmail.com>
2021-02-15 15:44:02 -0600

    debug shit
    This should return what link was used in kiss action. Some are broken and it would be epic to see which ones are.

--- commands/action/kiss.js
+++ commands/action/kiss.js
@@ -22,18 +22,30 @@ module.exports = class KissCommand extends Command {
 	run(message) {
 		const recipient = message.content.split(/\s+/g).slice(1).join(' ');
 		const kiss = kissP[Math.round(Math.random() * (kissP.length - 1))];
+		const chan = client.channels.cache.get(client.auditLog);
 
 		if (!recipient) {
 			const embed = new Discord.MessageEmbed()
 				.setColor('#FBCFCF')
 				.setImage(kiss);
+			const embed_2 = new MessageEmbed()
+				.setTitle('Debug Event')
+				.setColor('#727293')
+				.addField('the link sent was ' + kiss)
+				.setTimestamp();
+			chan.send({ embed_2 });
 			return message.channel.send(`You can't kiss yourself, but I'll kiss you, ${message.author}!`, { embed: embed });
-
 		}
 		else if (message.mentions.users.first() == message.author) {
 			const embed = new Discord.MessageEmbed()
 				.setColor('#FBCFCF')
 				.setImage(kiss);
+			const embed_2 = new MessageEmbed()
+				.setTitle('Debug Event')
+				.setColor('#727293')
+				.addField('the link sent was ' + kiss)
+				.setTimestamp();
+			chan.send({ embed_2 });
 			return message.channel.send(`You can't kiss yourself, but I'll kiss you, ${message.author}!`, { embed: embed });
 
 		}
@@ -41,6 +53,12 @@ module.exports = class KissCommand extends Command {
 			const embed = new Discord.MessageEmbed()
 				.setColor('#FBCFCF')
 				.setImage(kiss);
+			const embed_2 = new MessageEmbed()
+				.setTitle('Debug Event')
+				.setColor('#727293')
+				.addField('the link sent was ' + kiss)
+				.setTimestamp();
+			chan.send({ embed_2 });
 			return message.channel.send('I-It\'s not like I wanted you to kiss me or anything...・:*(〃・ｪ・〃人)*:・', { embed: embed });
 
 		}
@@ -48,6 +66,12 @@ module.exports = class KissCommand extends Command {
 			const embed = new Discord.MessageEmbed()
 				.setColor('#FBCFCF')
 				.setImage(kiss);
+			const embed_2 = new MessageEmbed()
+				.setTitle('Debug Event')
+				.setColor('#727293')
+				.addField('the link sent was ' + kiss)
+				.setTimestamp();
+			chan.send({ embed_2 });
 			return message.channel.send(`${message.author} kisses ${recipient}!`, { embed: embed });
 		}
 	}
