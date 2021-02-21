331f300a573067616debfa48afaaa83288ace688
Snoo <wm28744@gmail.com>
2021-02-21 00:54:20 -0600

    grope got simplified pog

--- commands/action/grope.js
+++ commands/action/grope.js
@@ -19,35 +19,24 @@ module.exports = class GropeCommand extends Command {
         const disgust =
             disgustP[Math.round(Math.random() * (disgustP.length - 1))];
         const grope = gropeP[Math.round(Math.random() * (gropeP.length - 1))];
-
-        if (!recipient) {
-            var embed = new Discord.MessageEmbed()
-                .setColor('#FBCFCF')
-                .setImage(disgust);
-            return message.channel.send(
-                `${message.author} gropes... themselves..?`,
-                { embed: embed }
-            );
-        } else if (message.mentions.users.first() == message.author) {
-            var embed = new Discord.MessageEmbed()
-                .setColor('#FBCFCF')
-                .setImage(disgust);
+        const embed = new Discord.MessageEmbed()
+        if (!recipient || message.mentions.users.first() == message.author) {
+            embed.setColor('#FBCFCF')
+            embed.setImage(disgust);
             return message.channel.send(
                 `${message.author} gropes... themselves..?`,
                 { embed: embed }
             );
         } else if (message.mentions.users.first() == this.client.user) {
-            const embed = new Discord.MessageEmbed()
-                .setColor('#FBCFCF')
-                .setImage(grope);
+            embed.setColor('#FBCFCF')
+            embed.setImage(grope);
             return message.channel.send(
                 "E-EH?! Nya! Where...are you.. touching.. (✿´ ꒳ ` ) I guess I'm okay with it as long as you are... Don't take this the wrong way!",
                 { embed: embed }
             );
         } else {
-            var embed = new Discord.MessageEmbed()
-                .setColor('#FBCFCF')
-                .setImage(grope);
+            embed.setColor('#FBCFCF')
+            embed.setImage(grope);
             return message.channel.send(
                 `${message.author} has started... groping ${recipient}?`,
                 { embed: embed }
