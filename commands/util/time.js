1ea1c27c4149138ea3fad3cf59b496729875a8d8
Sean Doran <sdoran35@gmail.com>
2022-12-15 19:44:18 -0500

    Fix Time Command

--- commands/util/time.js
+++ commands/util/time.js
@@ -1,21 +1,5 @@
 const Command = require('../../structures/Command');
 
-const moment = require('moment');
-const clocks = [
-    '🕛',
-    '🕐',
-    '🕑',
-    '🕒',
-    '🕓',
-    '🕔',
-    '🕕',
-    '🕖',
-    '🕗',
-    '🕘',
-    '🕙',
-    '🕚',
-];
-
 module.exports = class TimeCommand extends Command {
     constructor(client) {
         super(client, {
@@ -48,7 +32,7 @@ module.exports = class TimeCommand extends Command {
         }
 
         await this.apiReq
-            .get(`https://time.is/${location}`)
+            .get(`https://www.timeapi.io/api/Time/current/zone?timeZone=${location}`)
             .then(function (res) {
                 if (res.status !== 200) {
                     return message.channel.send(
@@ -57,33 +41,8 @@ module.exports = class TimeCommand extends Command {
                 }
 
                 const text = res.data;
-
-                const date = text.match(
-                    new RegExp(
-                        '<div id="dd" class="w90 tr" onclick="location=\'/calendar\'" ' +
-                            'title="Click for calendar">([^]+?)</div>'
-                    )
-                )[1];
-                const time = text
-                    .match(/<div id="twd">([^]+?)<\/div>/)[1]
-                    .replace(
-                        /<span id="ampm" style="font-size:21px;line-height:21px">(AM|PM)<\/span>/,
-                        ' $1'
-                    );
-                const place = text.match(
-                    /<div id="msgdiv"><h1>Time in ([^]+?) now<\/h1>/
-                )[1];
-                const clock = clocks[parseInt(time.split(':')[0], 10) % 12];
-
-                const parsedTime = moment(
-                    `${date} ${time}`,
-                    'dddd, MMMM D, YYYY HH:mm:ss A'
-                );
-                return message.channel.send(
-                    `${clock} | The time in **${place}** is \`${parsedTime.format(
-                        'dddd, MMMM Do YYYY @ h:mm:ss a'
-                    )}\`!`
-                );
+                const response = `The time in **${text.timeZone}** is ${text.time} on ${text.date}`;
+                return message.channel.send(response)
             })
             .catch(function (err) {
                 message.channel.send(
