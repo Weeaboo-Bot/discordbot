b383aacd7437a47b1c2f6685b01f5ecaac4d5f18
Sean Doran <sdoran35@gmail.com>
2023-06-27 18:02:21 -0400

    update messageDelete

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -44,7 +44,7 @@ module.exports = async (client, message) => {
             .addField('Delete Event', 'Member is Unknown')
             .setFooter(`v${version}`)
             .setTimestamp();
-    //    client.botLogger.send({ embed });
+        client.botLogger.send({ embed });
         client.logger.info(
             `A message by ${message.author.tag} was deleted, but we don't know by who.`
         );
