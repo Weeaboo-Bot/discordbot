dcf6e3e896bca497dc34180800517a35870b13aa
Sean Doran <sdoran35@gmail.com>
2024-02-22 18:40:32 -0500

    updates

--- Weaboo.js
+++ Weaboo.js
@@ -4,6 +4,21 @@ const Client = require('./structures/Client');
 
 global.__basedir = __dirname;
 
+// You can also use ESM `import * as Sentry from "@sentry/node"` instead of `require`
+const Sentry = require("@sentry/node");
+import { ProfilingIntegration } from "@sentry/profiling-node";
+
+Sentry.init({
+  dsn: "https://fcf71f3bff39a9628f404f08c83e1741@o4506793068527616.ingest.sentry.io/4506793068724224",
+  integrations: [
+    new ProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
+
 // Client setup
 const intents = new Discord.Intents();
 intents.add(
